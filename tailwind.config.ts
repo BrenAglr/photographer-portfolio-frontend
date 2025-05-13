a9f6bca8efcad70cbd4b7c0b64990fceb0e14d6e
BrenAglr <brenda.sthe.aguilar@gmail.com>
2025-05-13 20:20:33 -0300

    cambios de config

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        cerebri: ['Brasley', 'sans-serif'],
+      },
+      colors: {
+        velvet: "#222D52",
+        wheat: "#FDE5C3",
+        chocolate: "#500711",
+        powder: "#8DA9C4"
+      },
+      animation: {
+        'fade-in-down': 'fadeInDown 0.3s ease-out',
+      },
+      keyframes: {
+        fadeInDown: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
+
