25084b3259f060e72cfc1c0d5fccd85654513c7c
BrenAglr <brenda.sthe.aguilar@gmail.com>
2025-07-22 23:57:59 -0300

    actualizacion pageProps

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,1 +1,35 @@
-export { default } from '@/view/contact'
+import ContactForm from "@/components/contactForm"
+import ContactText from "@/components/contactText"
+import Image from "next/image"
+
+
+export const Contact: React.FC = () => {
+  return (
+   <section className="w-full min-h-[90vh] flex flex-col lg:flex-row px-8 py-8 gap-12 relative bg-cover bg-center bg-amber-100">
+       <Image
+            src="/photos/fondo1.jpg"
+            alt="Fondo"
+            fill
+            className="object-cover object-center z-0"
+            priority
+          />
+      {/* Contenedor oscuro sutil (opcional para contraste) */}
+      <div className="absolute inset-0 bg-white/50 backdrop-blur-[2px] z-0" />
+
+      {/* Contenido principal */}
+      <div className="relative z-10 w-full flex flex-col lg:flex-row items-start justify-between gap-12">
+        {/* Texto de contacto */}
+        <div className="w-full lg:w-1/2">
+          <ContactText />
+        </div>
+
+        {/* Formulario */}
+        <div className="w-full lg:w-1/2">
+          <ContactForm />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
--- src/app/sections/[section]/page.tsx
+++ src/app/sections/[section]/page.tsx
@@ -1,1 +1,22 @@
-export { default } from '@/view/section'
+import { SectionGalleryClient } from "@/components/sectionGalleryClient";
+import React from "react";
+
+interface PageProps {
+  params: Promise<{ section: string }>;
+}
+
+export const SectionGallery: React.FC<PageProps> = async ({params}: PageProps) => {
+    const resolvedParams = await params
+    const { section } = resolvedParams;
+
+  return (
+    <div className="max-w-6xl mx-auto px-4 py-8">
+      <h1 className="text-chocolate text-3xl font-bold mb-6 capitalize">
+        {section}
+      </h1>
+      <SectionGalleryClient section={section} />
+    </div>
+  );
+}
+
+export default SectionGallery
